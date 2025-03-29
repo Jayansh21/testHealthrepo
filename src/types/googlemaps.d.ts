6f13ba4b35adea48b64870f3eb0264276b3c611f
Ayush-upadhyay1605 <work.uayush16@gmail.com>
2025-03-29 09:56:23 +0000

    Reverted to edit edt-134cac83-9c8c-4d17-ab81-0303f34470da: "Fix TypeScript errors
    Fixes TypeScript errors in `src/mocks/browser.ts` and `src/pages/DoctorSearch.tsx`."

--- src/types/googlemaps.d.ts
+++ src/types/googlemaps.d.ts
@@ -1,4 +1,4 @@
 
 declare module '@react-google-maps/api/dist/utils/make-load-script-url' {
-  export type Libraries = Array<'places' | 'drawing' | 'geometry' | 'localContext' | 'visualization'>;
+  export type Libraries = Array<'places' | 'drawing' | 'geometry' | 'visualization'>;
 }
